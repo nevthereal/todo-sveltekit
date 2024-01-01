0be4c2901d8f9cf124ce06cc4cf7ec803cb1e207
Neville Brem <contact@nevillebrem.com>
2024-01-01 21:29:50 +0100

    fix: added orderby to todos

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,10 @@
 import { db } from '$lib/db/db';
 import { todosTable } from '$lib/db/schema';
-import { eq } from 'drizzle-orm';
+import { desc, eq } from 'drizzle-orm';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async () => {
-	const result = await db.select().from(todosTable);
+	const result = await db.select().from(todosTable).orderBy(desc(todosTable.id));
 	return {
 		result
 	};
@@ -13,7 +13,6 @@ export const load = (async () => {
 export const actions = {
 	createTodo: async ({ request }) => {
 		const formData = await request.formData();
-		console.log(formData);
 		const title = formData.get('title') as string;
 		const content = formData.get('content') as string;
 		await db.insert(todosTable).values({
