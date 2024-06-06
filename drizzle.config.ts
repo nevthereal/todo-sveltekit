ba37dad72c5f2ec7d52794d5cc3317e8a366cb08
Neville Brem <bremneville@gmail.com>
2024-06-06 06:59:25 +0200

    changed to local db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,10 @@
 import type { Config } from 'drizzle-kit';
-import 'dotenv/config';
-
-const { DB_URL, AUTH_TOKEN } = process.env;
 
 export default {
 	schema: './src/lib/db/schema.ts',
 	out: './migrations',
 	driver: 'turso',
 	dbCredentials: {
-		url: process.env.DB_URL!,
-		authToken: process.env.AUTH_TOKEN
+		url: 'file:local.db'
 	}
 } satisfies Config;
--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,6 +1,5 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
-import { DB_URL, AUTH_TOKEN } from '$env/static/private';
 
-const client = createClient({ url: DB_URL, authToken: AUTH_TOKEN });
+const client = createClient({ url: 'file:local.db' });
 export const db = drizzle(client);
