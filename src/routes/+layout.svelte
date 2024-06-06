a8f19767ed7866ac2834cc51869255eefc9be340
Neville Brem <bremneville@gmail.com>
2024-06-06 07:12:18 +0200

    added svelte query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import '../app.postcss';
+
+	const queryClient = new QueryClient();
 </script>
 
-<slot />
+<QueryClientProvider client={queryClient}>
+	<slot />
+</QueryClientProvider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,42 @@
 <script lang="ts">
 	import Todo from '$lib/components/Todo.svelte';
-	export let data;
-	let todos = data.result;
+	import { db } from '$lib/db/db';
+	import { todosTable } from '$lib/db/schema';
+	import { createQuery } from '@tanstack/svelte-query';
+	// export let data;
+	// let todos = data.result;
+
+	const query = createQuery({
+		queryKey: ['todos'],
+		queryFn: async () => {
+			const todos = await db.select().from(todosTable);
+			return todos;
+		}
+	});
 </script>
 
 <div class="w-[60%] mx-auto mt-16">
 	<h1 class="h1 mb-4">
-		Todos{#if todos.length}<span>{' '}({todos.length})</span>{/if}:
+		{#if $query.isLoading}
+			<span>Loading data...</span>
+		{:else if $query.isSuccess && $query.data}
+			Todos{#if $query.data.length}<span>{' '}({$query.data.length})</span>{/if}:
+		{/if}
 	</h1>
 	<form method="POST" class="mb-4 flex gap-2 text-black">
 		<input type="text" name="title" placeholder="Title" />
 		<input type="text" name="content" placeholder="What needs to be done?" />
 		<button class="btn variant-ghost-primary text-white">Add</button>
 	</form>
-
-	<ul class="flex flex-col gap-4">
-		{#each todos as todo (todo.id)}
-			<Todo {todo} />
-		{/each}
-	</ul>
+	{#if $query.isLoading}
+		<span>Loading todos...</span>
+	{:else if $query.error}
+		<span>Query failed</span>
+	{:else if $query.isSuccess && $query.data}
+		<ul class="flex flex-col gap-4">
+			{#each $query.data as todo (todo.id)}
+				<Todo {todo} />
+			{/each}
+		</ul>
+	{/if}
 </div>
