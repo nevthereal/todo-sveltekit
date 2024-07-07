61dcf6c923b774946a7f9ae0611e5732c73579bc
Neville Brem <bremneville@gmail.com>
2024-07-07 09:03:53 +0200

    local db

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
-const client = createClient({ url: 'file:local.db' });
+const client = createClient({ url: 'http://127.0.0.1:8080' });
 export const db = drizzle(client);
